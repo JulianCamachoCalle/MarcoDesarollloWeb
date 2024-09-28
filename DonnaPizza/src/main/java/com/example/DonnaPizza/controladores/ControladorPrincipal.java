4346d9cdf379b568cd8055e040cd2dcaae040829
JoMontoro <126255200+JoMontoro@users.noreply.github.com>
2024-09-28 16:44:35 -0500

    Update ControladorPrincipal.java

--- DonnaPizza/src/main/java/com/example/DonnaPizza/controladores/ControladorPrincipal.java
+++ DonnaPizza/src/main/java/com/example/DonnaPizza/controladores/ControladorPrincipal.java
@@ -3,6 +3,12 @@ package com.example.DonnaPizza.controladores;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import com.example.DonnaPiza.DAO.sugerenciasDAO;
+import com.example.DonnaPiza.Entity.sugerencias;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.validation.annotation.Validated;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class ControladorPrincipal {
@@ -31,10 +37,24 @@ public class ControladorPrincipal {
     public String login(Model model) {
         return "login";
     }
-    
+    sugerenciasDAO objSug=new sugerenciasDAO();
     @GetMapping("/sugerencias")
     public String sugerencias(Model model) {
         return "sugerencias";
+    }
+    @PostMapping("/registrar")
+    public String registarSugerencia(@Validated sugerencias sugerencia){
+        if(sugerencia!=null){ 
+            objSug.registrarSug(sugerencia);
+        }
+        return "sugerencias";
+    }
+    @GetMapping("/lista")
+    public String getLista(Model model){
+        List<sugerencias> listSug= new ArrayList<>();
+        listSug=objSug.getsugerencias();
+        model.addAttribute("listaA", listSug);
+        return "listasugerencias";
     }
     
     @GetMapping("/reclamaciones")
